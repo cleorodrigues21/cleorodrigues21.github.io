8b69f06d6c17516a87df6a79fa317134ce01fec7
cleorodrigues21 <cleorodrigues073@gmail.com>
2022-10-26 17:07:54 -0300

    atualizando caminhos

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
   <head>
     <title>Título da página</title>
     <meta charset="UTF8" />
-    <link rel="stylesheet" type="text/css" href="index.css" />
-    <link rel="stylesheet" type="text/css" href="./fontawesome/css/all.css"/>
-    <link rel="stylesheet" type="text/css" href="./footer/footer.css"/>
-    <link rel="stylesheet" type="text/css" href="./header/header.css"/>
+    <link rel="stylesheet" type="text/css" href="Pagina-Pessoal/fontawesome/css/all.css"/>
+    <link rel="stylesheet" type="text/css" href="Pagina-Pessoal/footer/footer.css"/>
+    <link rel="stylesheet" type="text/css" href="Pagina-Pessoal/header/header.css"/>
   </head>
   
-  <body><?php require_once "./header/header.php" ?>
+  <body><?php require_once "Pagina-Pessoal/header/header.php" ?>
   <br>
     <div style="text-align: center;margin-left: auto;margin-right: auto;width: 95%;height: 500px;background-color: #D3D3D3;" >
       <fieldset style="width: 97%;height: 480px;margin-left: auto;margin-right: auto;">
@@ -19,5 +18,5 @@
         </table>
       </fieldset>
     </div>
-  </body><?php require_once "./footer/footer.php" ?>
+  </body><?php require_once "Pagina-Pessoal/footer/footer.php" ?>
 </html>
